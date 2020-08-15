7251ebe70831463ba73eb8c2e1b33dac075ac10e
Lauren McCoy <lm_lutec@outlook.com>
2020-08-15 14:13:42 -0400

    owner params are permitted to create owner from frontend. Now moving to the next error: fetch request not returning any data

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           {/* Edit <code>src/App.js</code> and save to reload. */}
-          <OwnerContainer/>
         </p>
+        <OwnerContainer/>
         <a
           className="App-link"
           href="https://reactjs.org"
--- src/components/OwnerInput.js
+++ src/components/OwnerInput.js
@@ -4,50 +4,53 @@ import React, { Component } from 'react'
 class OwnerInput extends Component {
 
     state = {
-        firstName: "test",
-        lastName: "name",
-        email: "",
-        password: ""
+        owner: {
+            first_name: "",
+            last_name: "",
+            email: "",
+            password: ""
+        }
     }
 
-    onFirstNameChange = event => {
+    handleChange = event => {
         this.setState({
-            firstName: event.target.value
+            owner: {
+                ...this.state.owner,
+                [event.target.id]: event.target.value
+            }
         })
     }
 
-    onLastNameChange = event => {
-        this.setState({
-            lastName: event.target.value
-        })
-    }
 
-    onEmailChange = event => {
-        this.setState({
-            email: event.target.value
-        })
-    }
+    handleSubmit = event => {
+        event.preventDefault()
+        
+        const formData = {
+            method: 'POST',
+            headers: { 
+                'Content-Type': 'application/json'
+             },
+            body: JSON.stringify(this.state) 
+        }
 
-    onPasswordChange = event => {
-        this.setState({
-            password: event.target.value
-        })
+        fetch("http://localhost:3000/api/v1/owners/", formData)
+            .then(response => console.log(response.json()))
+            .then(json => console.log(json))
     }
-
 
     render(){
         return(
             <div>
                 Owner input is connected to the owner container
-                <form>
+                <form onSubmit={event => {this.handleSubmit(event)}}>
                     <label>First name</label>
-                    <input onChange= { event => {this.onFirstNameChange(event)}} type="text" name="firstName" value={this.state.firstName}/><br/>
+                    <input onChange= { event => {this.handleChange(event)}} type="text" id= "first_name" value={this.state.owner.first_name}/><br/>
                     <label>Last name</label>
-                    <input onChange={ event => {this.onLastNameChange(event)}} type="text" name="lastName" value={this.state.lastName} /><br/>
+                    <input onChange={ event => {this.handleChange(event)}} type="text" id="last_name" value={this.state.owner.last_name} /><br/>
                     <label>Email</label>
-                    <input onChange={ event => {this.onEmailChange(event)}} type="text" name="email" value={this.state.email} /><br/>
+                    <input onChange={ event => {this.handleChange(event)}} type="text" id="email" value={this.state.owner.email} /><br/>
                     <label>Password</label>
-                    <input onChange={ event => {this.onPasswordChange(event)}} type="password" name="password" value={this.state.password}/><br/>
+                    <input onChange={ event => {this.handleChange(event)}} type="password" id="password" value={this.state.owner.password}/><br/>
                     <input type="submit" name="submit"/>
                 </form>
             </div>
