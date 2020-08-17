c9f54593222dc3ae880309e6543ad6629ebbf7f2
Lauren McCoy <lm_lutec@outlook.com>
2020-08-17 09:18:25 -0400

    installed thunk

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 // import * as serviceWorker from './serviceWorker';
 
-const store = createStore()
+const store = createStore(applyMiddleware(thunk))
 
 ReactDOM.render(
   // <React.StrictMode>
