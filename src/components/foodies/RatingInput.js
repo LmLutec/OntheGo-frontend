175d13da6561877a8cc1adf61be9df1b686d2d77
Lauren McCoy <lm_lutec@outlook.com>
2020-10-25 15:10:59 -0400

    changed ratinginput form field types to select instead of text

--- src/components/foodies/RatingInput.js
+++ src/components/foodies/RatingInput.js
@@ -44,23 +44,56 @@ class RatingInput extends Component {
         })
 
         window.history.back()
-        // this.props.history.push("/home")
+    }
+
+    goBack = () => {
+        window.history.back()
     }
 
     render(){
         
         return(
             <div>
-                Rate Foodtruck below
+                Rate {this.props.truck.name}
                 <form onSubmit={(event) => {this.handleSubmit(event)}}>
-                    <label>On a scale of 1 to 5 (1 = bad, 5 = excellent), rate the following areas</label><br/>
-                    Food quality(Freshness and taste)<input onChange={(event) => {this.handleChange(event)}} type="text" id="food_quality" value={this.state.rating.food_quality}/><br/>
-                    Speed of Service<input onChange={(event) => {this.handleChange(event)}} type="text" id="speed_of_service" value={this.state.rating.speed_of_service}/><br/>
-                    Customer Service<input onChange={(event) => {this.handleChange(event)}} type="text" id="customer_service" value={this.state.rating.customer_service}/><br/>
-                    Price (1 = not expensive, 5 = expensive)<input onChange={(event) => {this.handleChange(event)}} type="text" id="prices" value={this.state.rating.prices}/><br/>
+                    <label>Rate the following areas</label><br/>
+                    Food quality(Freshness and taste)
+                    <select onChange={(event) => {this.handleChange(event)}} value={this.state.rating.food_quality} id="food_quality" required>
+                    <option value="1">Food was cold/not fresh and was not flavorful</option>
+                    <option value="2">Luke warm/cold food with little to no flavor</option>
+                    <option value="3">Warm food, seasoned well</option>
+                    <option value="4">Hot food, could use more flavor</option>
+                    <option value="5">Food was hot, fresh, and flavorful</option>
+                    </select><br/>
+                    Speed of Service
+                    <select onChange={(event) => {this.handleChange(event)}} value={this.state.rating.speed_of_service} id="speed_of_service" required>
+                    <option value="1">There was a long wait to order OR I had to wait over 30 minutes for my order</option>
+                    <option value="2">There was a long wait to order OR I had to wait over 20 minutes for my order</option>
+                    <option value="3">Including waiting in line to order and waiting for order to cook, I spent around 20 minutes here</option>
+                    <option value="4">Including waiting in line to order and waiting for order to be compleleted, I spent around 15 minutes here</option>
+                    <option value="5">Including waiting in line to order and waiting for order to be compleleted, I spent less than 15 minutes here</option>
+                    </select><br/>
+                    Customer Service
+                    <select onChange={(event) => {this.handleChange(event)}} value={this.state.rating.customer_service} id="customer_service" required>
+                    <option value="1">The employee(s) was rude and not helpful</option>
+                    <option value="2">The employee(s) seemed disengaged with minimal eye contact and little interest(had to keep repeating order and etc.)</option>
+                    <option value="3">The employee(s) was not rude, was not very engaging, but paid attention to my order(received my order correctly without having constant repeats)</option>
+                    <option value="4">The employee(s) was friendly and helpful for the most part</option>
+                    <option value="5">The employee(s) were very helpful and/or friendly</option>
+                    </select><br/>
+                    Prices
+                    <select onChange={(event) => {this.handleChange(event)}} value={this.state.rating.prices} id="prices" required>
+                    <option value="1"> more than $30 a meal</option>
+                    <option value="2">around $25 a meal</option>
+                    <option value="3">around $20 a meal</option>
+                    <option value="4">under $15 per meal</option>
+                    <option value="5">Under $10 per meal</option>
+                    </select><br/>
+                    
                     <input type="submit" value="Add rating"/>
                 </form>
 
+                <button onClick={this.goBack()}>Go back</button>
                 <section className="errors">
                     {errorMessage}
                 </section>
