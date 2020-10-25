383c8738aa247b843977e3832624dde822c1dc46
Lauren McCoy <lm_lutec@outlook.com>
2020-10-25 12:07:44 -0400

    added require to form fields of the ownerinput component

--- src/components/owners/OwnerInput.js
+++ src/components/owners/OwnerInput.js
@@ -42,13 +42,13 @@ class OwnerInput extends Component {
                <h3>Hey Truck Owners! Create a New Account below:</h3>
                 <form onSubmit={event => {this.handleSubmit(event)}}> 
                     <label>First name</label>
-                    <input onChange= { event => {this.handleChange(event)}} type="text" id= "first_name" value={this.state.owner.first_name}/><br/>
+                    <input onChange= { event => {this.handleChange(event)}} type="text" id= "first_name" value={this.state.owner.first_name} required/><br/>
                     <label>Last name</label>
-                    <input onChange={ event => {this.handleChange(event)}} type="text" id="last_name" value={this.state.owner.last_name} /><br/>
+                    <input onChange={ event => {this.handleChange(event)}} type="text" id="last_name" value={this.state.owner.last_name} required/><br/>
                     <label>Email</label>
-                    <input onChange={ event => {this.handleChange(event)}} type="text" id="email" value={this.state.owner.email} /><br/>
+                    <input onChange={ event => {this.handleChange(event)}} type="text" id="email" value={this.state.owner.email} required/><br/>
                     <label>Password</label>
-                    <input onChange={ event => {this.handleChange(event)}} type="password" id="password" value={this.state.owner.password}/><br/>
+                    <input onChange={ event => {this.handleChange(event)}} type="password" id="password" value={this.state.owner.password} required/><br/>
                     <input type="submit" name="submit"/>
                 </form>
                 <br/>
