75a0cfd66b52f61fee67c6477618221c0f7a015b
Lauren McCoy <lm_lutec@outlook.com>
2020-10-29 13:24:16 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import logo from './logo.svg';
 import logo from './foodtruck.png'
 import './App.css';
 import OwnerContainer from './containers/OwnerContainer'
@@ -12,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" /><br/><br/>
    
 
        
