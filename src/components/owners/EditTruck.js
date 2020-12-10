5c14d8bf034ca3dfa6fab2b2fd46bb79af455cac
Lauren McCoy <lm_lutec@outlook.com>
2020-12-10 18:13:41 -0500

    changed arrangement of phone number fields

--- src/components/owners/EditTruck.js
+++ src/components/owners/EditTruck.js
@@ -115,8 +115,7 @@ class EditTruck extends Component {
                     <input onChange= { event => {this.handleChange(event)}} type="text" id="food_type" value={this.state.foodtruck.food_type} required/><br/>
                     <label>Phone number</label>  
                         (<input onChange={ event => {this.handlePhoneNumber(event)}} type="text" id="areaCode" value={this.state.numberParts.areaCode} style={{ width: "25px" }} required/>)
-                    <input onChange={ event => {this.handlePhoneNumber(event)}} type="text" id="begNum" value={this.state.numberParts.begNum} style={{ width: "25px" }} required/> -
-                    <input onChange={ event => {this.handlePhoneNumber(event)}} type="text" id="lastNum" value={this.state.numberParts.lastNum} style={{ width: "30px" }} required/><br/>
+                    <input onChange={ event => {this.handlePhoneNumber(event)}} type="text" id="begNum" value={this.state.numberParts.begNum} style={{ width: "25px" }} required/>-<input onChange={ event => {this.handlePhoneNumber(event)}} type="text" id="lastNum" value={this.state.numberParts.lastNum} style={{ width: "30px" }} required/><br/>
                     {/* <input onChange= { event => {this.handleChange(event)}} type="text" id="phone_number" value={this.state.foodtruck.phone_number} required/><br/> */}
                     <input type="submit" value="Edit Truck"/>
                 </form>
