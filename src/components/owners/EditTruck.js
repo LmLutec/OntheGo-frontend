3a1665970863cee9cb37c11c526f4fc9f4e42c77
Lauren McCoy <lm_lutec@outlook.com>
2021-02-23 15:52:36 -0500

    testing the whole number as a string

--- src/components/owners/EditTruck.js
+++ src/components/owners/EditTruck.js
@@ -80,7 +80,7 @@ class EditTruck extends Component {
             }
         })
         let number = `${this.state.numberParts.areaCode}` + `${this.state.numberParts.begNum}` + `${this.state.numberParts.lastNum}`
-        console.log(`${this.state.numberParts.begNum}`)
+        console.log(number)
         this.setState({
             foodtruck: {
                 foodtruck: {
