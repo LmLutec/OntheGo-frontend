37eeec8a522eedc370e43a624479dd4d2a023085
Lauren McCoy <lm_lutec@outlook.com>
2021-02-28 16:51:43 -0500

    update

--- src/components/owners/Home.js
+++ src/components/owners/Home.js
@@ -87,7 +87,7 @@ class Home extends Component {
                         <h3>Notes</h3>
                         {renderNotes()}
                     </section>
-                    <button onClick={this.addNote}>Add note</button><br/>
+                    <button onClick={this.addNote}>Add note</button>
                 </div>
                 <section className="menu_info full-menu">
                     <h3>Menu</h3>
