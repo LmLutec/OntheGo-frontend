eb1decb4d3f79849904f6b13a574df864edeb679
DevMcCoy <46851418+LmLutec@users.noreply.github.com>
2021-11-09 14:36:56 -0500

    added flex to different divs to center all elements on the landing page. changed some css colors

--- src/App.js
+++ src/App.js
@@ -1,33 +1,29 @@
-import React from 'react';
-import logo from './foodtruck.png'
-import './App.css';
-import OwnerContainer from './containers/OwnerContainer'
-import FoodieContainer from './containers/FoodieContainer'
-
-
-
-
+import React from "react";
+import logo from "./foodtruck.png";
+import "./App.css";
+import OwnerContainer from "./containers/OwnerContainer";
+import FoodieContainer from "./containers/FoodieContainer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Always on the go</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      
-      <section className="main">
-        <FoodieContainer />
-        <OwnerContainer />
-      </section>
-      
-     
-    
+      <div className="appWrap">
+        <header className="App-header">
+          <h1>Always On the Go</h1>
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
 
-
+        <section className="main">
+          <div className="container">
+            <FoodieContainer />
+          </div>
+          <div className="container">
+            <OwnerContainer />
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
-
 
 export default App;
